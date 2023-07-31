fa135f030a6177ad798115f5ad49bd9b8b5d40c3
codeenvo <codeenvo@gmail.com>
2023-08-01 00:15:48 +0530

    rahul finale

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export {default as Home} from './home/index';
+export {default as Gallery} from './gallery/index';
+export {default as Sermons} from './sermons/index';
+export { default as Contact } from './contact/index';
+export {default as Blog} from './blog/index';
+export { default as About } from "./about/index";
+export {default as Video} from './videos/index';
